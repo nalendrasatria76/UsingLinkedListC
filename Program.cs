c356f06d213a0b4a1868be4abefacc171c0a6929
TC1005 <curvafans383@gmail.com>
2022-11-30 16:23:53 +0700

    Menambahkan static void dan pilihan menu

--- Program.cs
+++ Program.cs
@@ -73,7 +73,27 @@ class Stacks
             char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
             switch (ch)
             {
-
+                case '1':
+                    Console.Write("\nEnter a number: ");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    s.push(num);
+                    break;
+                 case '2':
+                    if (s.empty())
+                    {
+                        Console.WriteLine("\nStack Empty");
+                    break;
+                    }
+                    s.pop();
+                    break;
+                  case '3':
+                    s.display();
+                    break;
+                  case '4':
+                      return;
+                    default:
+                       Console.WriteLine("\nInvalid Choice");
+                       break;
             }
         }
     }
