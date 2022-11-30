4f38c919714a8dfbde8f6b56818c544b714834ae
TC1005 <curvafans383@gmail.com>
2022-11-30 15:45:23 +0700

    Menambahkan public void push

--- Program.cs
+++ Program.cs
@@ -28,4 +28,13 @@ class Stacks
         else
             return (false);
     }
+    public void push(int element)
+    {
+        Node fresh;
+        fresh = new Node(element, null);
+
+        fresh.next = top;
+        top = fresh;
+        Console.WriteLine("\n" + element + "pushed.");
+    }
 }
